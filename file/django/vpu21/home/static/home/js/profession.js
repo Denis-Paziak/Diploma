2fb5e96c837a05fe7ab9392ed4e6d96ef8ee3155
Denis-Paziak <denispaziak@gmail.com>
2022-06-03 15:46:39 +0300

    debug

--- file/django/vpu21/home/static/home/js/profession.js
+++ file/django/vpu21/home/static/home/js/profession.js
@@ -1,10 +1,9 @@
-let but_close_info_block = document.querySelector('.info_profession .close');
-let but_open_info_block = document.querySelectorAll('.box ');
+let but_close_info_block = document.querySelector('.my_info_profession .close');
+let but_open_info_block = document.querySelectorAll('.box .open_info');
 
-let info_block = document.querySelector('.info_profession');
+let info_block = document.querySelector('.my_info_profession');
 let info_block_text = document.querySelector('.info_block_text');
 let info_block_img = document.querySelector('.info_block_img');
-
 
 but_open_info_block.forEach(element => {
     element.addEventListener("click", open_info);
@@ -14,24 +13,19 @@ but_close_info_block.addEventListener("click", close_info);
 
 
 function close_info() {
-    info_block.style.opacity = "0";
+    document.body.style.overflow = "visible";
     info_block.style.left = "-100%";
-    document.querySelector(".profession_container__box").style.display = "block";
 }
+
 function open_info(e) {
+    document.body.style.overflow = "hidden";
+
     let parent_el = e.path[3];
+
     let text = parent_el.querySelector(".text p").innerHTML;
     let img = parent_el.querySelector("img");
 
     info_block_img.src = img.src;
     info_block_text.innerHTML = text;
-    info_block.style.opacity = "1";
     info_block.style.left = "0%";
-
-    if (window.innerWidth <= 600) {
-        setTimeout(() => {
-            window.scrollTo(0, 0);
-            document.querySelector(".profession_container__box").style.display = "none";
-        }, 600);
-    }
 }
